9f4c16f766e27f19fb5866ba91ab2a6558cfd035
Maksim Lyashkogit config --global user.email maksikif1@gmail.com <166531241+Makskif@users.noreply.github.com>
2024-06-09 07:50:24 +0300

    dz2

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,14 @@
 function checkForSpam(message) {
 
     let mes = message.toLowerCase();
-    console.log(mes);
-    console.log(mes.includes('sale') || mes.includes('spam'));
+    
+    return mes.includes('sale') || mes.includes('spam');
 }
  
-checkForSpam("Latest technology news"); // false
-checkForSpam("JavaScript weekly newsletter"); // false
-checkForSpam("Get best sale offers now!"); // true
-checkForSpam("Amazing SalE, only tonight!"); // true
-checkForSpam("Trust me, this is not a spam message"); // true
-checkForSpam("Get rid of sPaM emails. Our book in on sale!"); // true
-checkForSpam("[SPAM] How to earn fast money?"); // true
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
